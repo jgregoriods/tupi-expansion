5eee77118e8d0d29dac867e5c3b8c6de92f929cb
jgregoriods <jonas.gregorio@gmail.com>
2021-03-30 16:49:02 +0200

    add asympt dispersal model

--- model.py
+++ model.py
@@ -2,10 +2,19 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pyproj
 
+from tqdm import tqdm
+
 
 albers = pyproj.Proj("+proj=aea +lat_1=-5 +lat_2=-42 +lat_0=-32 +lon_0=-60 \
                       +x_0=0 +y_0=0 +ellps=aust_SA +towgs84=-57,1,-41,0,0,0,0 \
                       +units=m +no_defs")
+CELL_AREA = 2500
+rmax = 0.04
+K = 1 * CELL_AREA
+phi = 0.75
+
+
+mask = [(-1, -1), (0, -1), (1, -1), (-1, 0), (1, 0), (-1, 1), (0, 1), (1, 1)]
 
 
 def to_grid(x, y):
@@ -16,10 +25,12 @@ def to_grid(x, y):
 
 class Model:
     def __init__(self):
+        self.date = 6000
         self.grid = {}
         self.settled_cells = []
         self.setup_layers()
         self.setup_population()
+
     def setup_layers(self):
         elevation = np.loadtxt('layers/ele.asc', skiprows=6)
         vegetation = np.loadtxt('layers/veg/veg_6000.asc', skiprows=6)
@@ -27,31 +38,65 @@ class Model:
             for col in range(128):
                 self.grid[(col, row)] = {'elevation': elevation[row, col],
                                          'population': 0,
-                                         'vegetation': vegetation[row, col]}
+                                         'vegetation': vegetation[row, col],
+                                         'arrival_time': 0}
+
     def setup_population(self):
         start_coords = to_grid(-61.95, -10.95)
         self.grid[start_coords]['population'] = 100
+        self.grid[start_coords]['arrival_time'] = self.date
         self.settled_cells.append(start_coords)
+
     def grow_population(self):
-        for cell in self.settled_cells:
-            self.grid[cell]['population'] += 10
+        for cell in self.settled_cells.copy():
+            N = self.grid[cell]['population']
+            self.grid[cell]['population'] += rmax * ((K-N)/K) * N
+
     def disperse_population(self):
-        for cell in self.settled_cells:
-            if self.grid[cell]['population'] > 150:
-                pass
+        for cell in self.settled_cells.copy():
+            N = self.grid[cell]['population']
+            if N / K > phi:
+                migrants = N * (1 - (phi / (N / K)))
+                neighbor_cells = self.get_neighbor_cells(cell)
+                if neighbor_cells:
+                    chosen_cell = neighbor_cells[np.random.choice(list(range(len(neighbor_cells))))]
+                    if self.grid[chosen_cell]['population'] == 0:
+                        self.settled_cells.append(chosen_cell)
+                        self.grid[chosen_cell]['arrival_time'] = self.date
+                    self.grid[cell]['population'] -= migrants
+                    self.grid[chosen_cell]['population'] += migrants
+
+    def get_neighbor_cells(self, cell):
+        x, y = cell
+        neighbor_cells = []
+        # for i in range(-1, 2):
+        #    for j in range(-1, 2):
+        for (i, j) in mask:
+            new_cell = (x+i, y+j)
+            # if (new_cell != (0, 0) and new_cell in self.grid and
+            if (new_cell in self.grid and
+                    self.grid[new_cell]['elevation'] > 0 and
+                    self.grid[new_cell]['population'] < phi * K):
+                neighbor_cells.append(new_cell)
+        return neighbor_cells
 
     def run(self):
         self.grow_population()
+        self.disperse_population()
+        self.date -= 1
 
 
 if __name__ == '__main__':
     m = Model()
-    for i in range(10):
+    for i in tqdm(range(3000)):
         m.run()
     p = np.zeros((165, 128))
     for row in range(165):
-            for col in range(128):
-                p[row][col] = m.grid[(col, row)]['population']
-    p[p==-9999] = np.nan
+        for col in range(128):
+            p[row][col] = m.grid[(col, row)]['arrival_time']
+    p[p == 0] = np.nan
     plt.imshow(p)
     plt.show()
+    #f = open('arr.asc', 'w')
+    #np.savetxt(f, p)
+    #f.close()
