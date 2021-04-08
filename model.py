fca588cc8c92c1930307839d96263c4b64846dfa
jgregoriods <jonas.gregorio@gmail.com>
2021-04-08 19:05:57 +0200

    minor changes

--- model.py
+++ model.py
@@ -12,10 +12,12 @@ albers = pyproj.Proj("+proj=aea +lat_1=-5 +lat_2=-42 +lat_0=-32 +lon_0=-60 \
                       +x_0=0 +y_0=0 +ellps=aust_SA +towgs84=-57,1,-41,0,0,0,0 \
                       +units=m +no_defs")
 CELL_AREA = 2500
+HEADER = np.loadtxt('layers/ele.asc', dtype='U', max_rows=6)
+HEADER = '\n'.join([' '.join(x) for x in HEADER])
 
 
 def get_distance(a, b):
-    return round(np.hypot((a[0] - b[0]), (a[1] - b[1])))
+    return np.hypot((a[0] - b[0]), (a[1] - b[1]))
 
 
 def to_grid(cell):
@@ -27,7 +29,7 @@ def to_grid(cell):
 
 mask = [(-1, -1), (0, -1), (1, -1), (-1, 0), (1, 0), (-1, 1), (0, 1), (1, 1)]
 leap_mask = [(i, j) for i in range(-3, 4) for j in range(-3, 4)
-             if (i, j) != (0, 0) and get_distance((0, 0), (i, j)) <= 3]
+             if (i, j) != (0, 0) and round(get_distance((0, 0), (i, j))) <= 3]
 
 
 class Model:
@@ -63,33 +65,32 @@ class Model:
         self.grid[start_coords]['arrival_time'] = self.date
         self.settled_cells.append(start_coords)
 
-    def grow_population(self):
-        for cell in self.settled_cells.copy():
-            N = self.grid[cell]['population']
-            self.grid[cell]['population'] += self.r * (1 - (N / self.K)) * N
+    def grow_population(self, cell):
+        #for cell in self.settled_cells.copy():
+        N = self.grid[cell]['population']
+        self.grid[cell]['population'] += self.r * (1 - (N / self.K)) * N
 
-    def disperse_population(self):
-        for cell in self.settled_cells.copy():
-            N = self.grid[cell]['population']
-            if N / self.K > self.C:
-                migrants = N * (1 - (self.C / (N / self.K)))
-                #migrants = N * 0.1 * (N / self.K)
-                neighbor_cells = self.get_neighbor_cells(cell)
-                if neighbor_cells:
-                    chosen_cell = neighbor_cells[np.random.choice(list(range(len(neighbor_cells))))]
-                    if self.grid[chosen_cell]['vegetation'] < self.forest:
-                        chosen_cell = self.leap(cell)
-                    if self.grid[chosen_cell]['population'] == 0:
-                        self.settled_cells.append(chosen_cell)
-                        self.grid[chosen_cell]['arrival_time'] = self.date
-                    self.grid[cell]['population'] -= migrants
-                    self.grid[chosen_cell]['population'] += migrants
+    def disperse_population(self, cell):
+        #for cell in self.settled_cells.copy():
+        N = self.grid[cell]['population']
+        if N / self.K > self.C:
+            #migrants = (1 - (self.C / (N / self.K))) * N
+            migrants = self.C * ((N / self.K)**10) * N
+            neighbor_cells = self.get_neighbor_cells(cell)
+            if neighbor_cells:
+                chosen_cell = neighbor_cells[np.random.choice(list(range(len(neighbor_cells))))]
+                if self.grid[chosen_cell]['vegetation'] < self.forest:
+                    chosen_cell = self.leap(cell)
+                if self.grid[chosen_cell]['population'] == 0:
+                    self.settled_cells.append(chosen_cell)
+                    self.grid[chosen_cell]['arrival_time'] = self.date
+                self.grid[cell]['population'] -= migrants
+                self.grid[chosen_cell]['population'] += migrants
 
     def leap(self, cell):
-        x, y = cell
         leap_cells = []
         for (i, j) in leap_mask:
-            new_cell = (x+i, y+j)
+            new_cell = (cell[0]+i, cell[1]+j)
             if (new_cell in self.grid and
                     self.grid[new_cell]['vegetation'] >= self.forest and
                     0 < self.grid[new_cell]['elevation'] < 1000 and
@@ -102,10 +103,9 @@ class Model:
             return cell
 
     def get_neighbor_cells(self, cell):
-        x, y = cell
         neighbor_cells = []
         for (i, j) in mask:
-            new_cell = (x+i, y+j)
+            new_cell = (cell[0]+i, cell[1]+j)
             if (new_cell in self.grid and
                     #self.grid[new_cell]['vegetation'] >= self.forest and
                     0 < self.grid[new_cell]['elevation'] < 1000 and
@@ -134,13 +134,14 @@ class Model:
     def run(self, num_iter):
         for i in tqdm(range(num_iter)):
             self.update()
-            self.grow_population()
-            self.disperse_population()
+            for cell in self.settled_cells.copy():
+                self.grow_population(cell)
+                self.disperse_population(cell)
             self.date -= 1
 
 
 if __name__ == '__main__':
-    m = Model(5000, (-61.96, -10.96), 0.02, 1, 0.5, 0)
+    m = Model(5000, (-61.96, -10.96), 0.025, 1, 0.25, 0)
     m.run(2000)
     p = np.zeros((165, 128))
     for row in range(165):
@@ -150,6 +151,4 @@ if __name__ == '__main__':
     plt.imshow(p)
     plt.show()
     print(m.score('tupi_filtered.csv'))
-    """f = open('arr.asc', 'w')
-    np.savetxt(f, p)
-    f.close()"""
+    #np.savetxt('arr.asc', p, header=HEADER, comments='')
