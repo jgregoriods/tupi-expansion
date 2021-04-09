c1a5ffe8c9ac49d03ef96c13056b1de223dc9650
jgregoriods <jonas.gregorio@gmail.com>
2021-04-09 16:03:02 +0200

    back to 50 km neighborhood

--- eco.R
+++ eco.R
@@ -7,8 +7,19 @@ get_ecotone <- function(r) {
   return(eco)
 }
 
-for(i in 1000*1:6) {
-  veg <- raster(paste("veg_", i, ".asc", sep=""))
-  eco <- get_ecotone(veg)
-  writeRaster(eco, paste("eco_", i, ".asc", sep=""))
+run_eco_files <- function(){
+  for(i in 1000*1:6) {
+    veg <- raster(paste("veg_", i, ".asc", sep=""))
+    eco <- get_ecotone(veg)
+    writeRaster(eco, paste("eco_", i, ".asc", sep=""))
+  }
+}
+
+eco_forest_cells <- function() {
+  for(i in 1000*1:6) {
+    veg <- raster(paste("veg_", i, ".asc", sep=""))
+    eco <- raster(paste("eco_", i, ".asc", sep=""))
+    veg[values(veg)==1 & values(eco)==1] <- 2
+    writeRaster(veg, paste("veg2_", i, ".asc", sep=""))
+  }
 }
--- model.py
+++ model.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 import pyproj
 import seaborn as sns
+import statsmodels.formula.api as smf
 
 from tqdm import tqdm
 
@@ -12,8 +13,9 @@ albers = pyproj.Proj("+proj=aea +lat_1=-5 +lat_2=-42 +lat_0=-32 +lon_0=-60 \
                       +x_0=0 +y_0=0 +ellps=aust_SA +towgs84=-57,1,-41,0,0,0,0 \
                       +units=m +no_defs")
 CELL_AREA = 2500
-HEADER = np.loadtxt('layers/ele.asc', dtype='U', max_rows=6)
-HEADER = '\n'.join([' '.join(x) for x in HEADER])
+HEADER = '\n'.join([' '.join(x) for x in np.loadtxt('layers/ele.asc', dtype='U',
+                                                    max_rows=6)])
+SITES = pd.read_csv('tupi.csv')
 
 
 def get_distance(a, b):
@@ -27,18 +29,21 @@ def to_grid(cell):
     return x_grid, y_grid
 
 
-mask = [(-1, -1), (0, -1), (1, -1), (-1, 0), (1, 0), (-1, 1), (0, 1), (1, 1)]
+mask = [(-1,-1), (0,-1), (1,-1),
+        (-1, 0),         (1, 0),
+        (-1, 1), (0, 1), (1, 1)]
+
 leap_mask = [(i, j) for i in range(-3, 4) for j in range(-3, 4)
              if (i, j) != (0, 0) and round(get_distance((0, 0), (i, j))) <= 3]
 
 
 class Model:
-    def __init__(self, start_date, start_coords, r, K, C, forest):
+    def __init__(self, start_date, start_coords, r, C, forest):
         self.date = start_date
         self.start_coords = start_coords
 
         self.r = r
-        self.K = K * CELL_AREA
+        self.K = CELL_AREA
         self.C = C
         self.forest = forest
 
@@ -49,8 +54,8 @@ class Model:
 
     def setup_layers(self):
         elevation = np.loadtxt('layers/ele.asc', skiprows=6)
-        start_date = int(np.ceil(5900/1000) * 1000)
-        vegetation = np.loadtxt(f'layers/veg/veg_{start_date}.asc', skiprows=6)
+        start_date = int(np.ceil(self.date / 1000) * 1000)
+        vegetation = np.loadtxt(f'layers/veg/veg2_{start_date}.asc', skiprows=6)
         for row in range(165):
             for col in range(128):
                 self.grid[(col, row)] = {'elevation': elevation[row, col],
@@ -66,48 +71,45 @@ class Model:
         self.settled_cells.append(start_coords)
 
     def grow_population(self, cell):
-        #for cell in self.settled_cells.copy():
         N = self.grid[cell]['population']
         self.grid[cell]['population'] += self.r * (1 - (N / self.K)) * N
 
     def disperse_population(self, cell):
-        #for cell in self.settled_cells.copy():
         N = self.grid[cell]['population']
         if N / self.K > self.C:
-            #migrants = (1 - (self.C / (N / self.K))) * N
-            migrants = self.C * ((N / self.K)**10) * N
+            migrants = (1 - (self.C / (N / self.K))) * N
+            chosen_cell = None
             neighbor_cells = self.get_neighbor_cells(cell)
             if neighbor_cells:
                 chosen_cell = neighbor_cells[np.random.choice(list(range(len(neighbor_cells))))]
-                if self.grid[chosen_cell]['vegetation'] < self.forest:
-                    chosen_cell = self.leap(cell)
+            elif self.forest and self.grid[cell]['vegetation'] == 2:
+                leap_cells = self.get_leap_cells(cell)
+                if leap_cells:
+                    chosen_cell = leap_cells[np.random.choice(list(range(len(leap_cells))))]
+            if chosen_cell is not None:
                 if self.grid[chosen_cell]['population'] == 0:
                     self.settled_cells.append(chosen_cell)
                     self.grid[chosen_cell]['arrival_time'] = self.date
                 self.grid[cell]['population'] -= migrants
                 self.grid[chosen_cell]['population'] += migrants
 
-    def leap(self, cell):
+    def get_leap_cells(self, cell):
         leap_cells = []
         for (i, j) in leap_mask:
             new_cell = (cell[0]+i, cell[1]+j)
             if (new_cell in self.grid and
-                    self.grid[new_cell]['vegetation'] >= self.forest and
+                    self.grid[new_cell]['vegetation'] == 2 and
                     0 < self.grid[new_cell]['elevation'] < 1000 and
                     self.grid[new_cell]['population'] < self.C * self.K):
                 leap_cells.append(new_cell)
-        if leap_cells:
-            chosen_cell = leap_cells[np.random.choice(list(range(len(leap_cells))))]
-            return chosen_cell
-        else:
-            return cell
+        return leap_cells
 
     def get_neighbor_cells(self, cell):
         neighbor_cells = []
         for (i, j) in mask:
             new_cell = (cell[0]+i, cell[1]+j)
             if (new_cell in self.grid and
-                    #self.grid[new_cell]['vegetation'] >= self.forest and
+                    self.grid[new_cell]['vegetation'] >= self.forest and
                     0 < self.grid[new_cell]['elevation'] < 1000 and
                     self.grid[new_cell]['population'] < self.C * self.K):
                 neighbor_cells.append(new_cell)
@@ -115,21 +117,23 @@ class Model:
 
     def update(self):
         if self.forest and not self.date % 1000:
-            vegetation = np.loadtxt(f'layers/veg/veg_{self.date}.asc',
+            vegetation = np.loadtxt(f'layers/veg/veg2_{self.date}.asc',
                                     skiprows=6)
             for cell in self.grid:
                 self.grid[cell]['vegetation'] = vegetation[cell[1]][cell[0]]
 
-    def score(self, filename):
-        dates = pd.read_csv(filename)
-        coords = list(zip(dates['x'], dates['y']))
-        real_ages = dates['bp']
-        dists = dates['dist']
-        sim_dates = [self.grid[to_grid(coord)]['arrival_time'] for coord in coords]
-        plt.scatter(dists, real_ages)
-        plt.scatter(dists, sim_dates)
+    def score(self, sites):
+        coords = list(zip(sites['x'], sites['y']))
+        sites['sim_dates'] = [self.grid[to_grid(coord)]['arrival_time']
+                              for coord in coords]
+        mod = smf.quantreg('bp ~ dist', data=sites)
+        res = mod.fit(q = 0.95)
+        line = res.predict(sites['dist'])
+        plt.scatter(sites['dist'], sites['bp'])
+        plt.plot(sites['dist'], line)
+        plt.scatter(sites['dist'][sites['sim_dates'] != 0],
+                    sites['sim_dates'][sites['sim_dates'] != 0])
         plt.show()
-        return sim_dates
 
     def run(self, num_iter):
         for i in tqdm(range(num_iter)):
@@ -138,17 +142,18 @@ class Model:
                 self.grow_population(cell)
                 self.disperse_population(cell)
             self.date -= 1
-
+            if not self.date % 500:
+                p = np.zeros((165, 128))
+                for row in range(165):
+                    for col in range(128):
+                        p[row][col] = m.grid[(col, row)]['arrival_time']
+                p[p == 0] = np.nan
+                plt.imshow(p)
+                plt.show()
 
 if __name__ == '__main__':
-    m = Model(5000, (-61.96, -10.96), 0.025, 1, 0.25, 0)
-    m.run(2000)
-    p = np.zeros((165, 128))
-    for row in range(165):
-        for col in range(128):
-            p[row][col] = m.grid[(col, row)]['arrival_time']
-    p[p == 0] = np.nan
-    plt.imshow(p)
-    plt.show()
-    print(m.score('tupi_filtered.csv'))
-    #np.savetxt('arr.asc', p, header=HEADER, comments='')
+    start_date = 5000
+    start_coords = (-61.96, -10.96)
+    m = Model(start_date, start_coords, 0.02, 0.75, 1)
+    m.run(start_date - 500)
+    m.score(SITES)
