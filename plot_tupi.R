e911c6ce1848a879c7f6a851b94b5d779f222cc7
jgregoriods <jonas.gregorio@gmail.com>
2021-05-14 17:33:40 +0200

    replace tupi map

--- plot_tupi.R
+++ plot_tupi.R
@@ -11,7 +11,9 @@ world <- ne_countries(scale=50, returnclass="sf")
 coast <- ne_coastline(scale=50, returnclass="sf")
 coast <- st_crop(coast, xmin=-85, xmax=-33, ymin=-60, ymax=12)
 sam <- st_crop(world, xmin=-85, xmax=-33, ymin=-60, ymax=12)
-tp <- read_sf("shp/tupi_languages.shp")
+#tp <- read_sf("shp/tupi_languages.shp")
+tp <- read_sf("shp/other_tupi.shp")
+tg <- read_sf("shp/tupi_guarani.shp")
 
 sites <- read.csv("sites/tupi_all.csv")
 sites$size <- 1
@@ -32,14 +34,16 @@ plt_a <- ggplot() +
 
 plt_b <- ggplot() +
     geom_sf(data=sam, fill="white", color=NA) +
-    geom_sf(data=tp, aes(fill="coral"), color=NA) +
+    geom_sf(data=tg, aes(fill="coral"), color=NA) +
+    geom_sf(data=tp, aes(fill="mediumpurple"), color=NA) +
     geom_sf(data=coast, fill=NA, color="black", size=0.2) +
     theme(axis.title.x=element_blank(), axis.title.y=element_blank(),
           panel.background = element_rect(fill = "aliceblue"),
           legend.position="bottom", legend.title = element_blank(),
+          legend.text=element_text(size=8),
           panel.border = element_rect(colour = "black", fill=NA),
           plot.title=element_text(size=16)) +
-    scale_fill_identity(labels=c("Tupi languages"), guide = "legend") +
+    scale_fill_identity(labels=c("Tupi-Guarani languages", "Other Tupi branches"), guide = "legend") +
     labs(title='b')
 
 grid.arrange(plt_a, plt_b, ncol = 2)
