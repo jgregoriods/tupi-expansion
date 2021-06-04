0f6064dab9956d09eb0ea192696f04501ae2c320
jgregoriods <jonas.gregorio@gmail.com>
2021-06-04 15:57:24 +0200

    refactor var names in model, add comments

--- main.py
+++ main.py
@@ -12,43 +12,46 @@ ORIGIN = (-61.96, -10.96)
 
 
 def test_model(params):
-    m = Model(5000, ORIGIN, params[0], params[1], params[2])
-    m.run()
-    m.get_score(SITES)
+    """
+    Runs simulations with different parameter combinations and calculates their
+    score.
+    """
+    model = Model(5000, ORIGIN, params[0], params[1], params[2])
+    model.run()
+    model.get_score(SITES)
     model_type = 'null' if not params[2] else 'forest'
-    return {'r': params[0], 'eK': params[1], 'model': model_type, 'score': m.score}
+    return {'growth': params[0], 'emigration': params[1], 'model': model_type, 'score': model.score}
 
 
 def main():
-    r_vals = np.arange(0.02, 0.045, 0.005)
-    e_vals = np.arange(0.2, 0.45, 0.05)
+    growth_vals = np.arange(0.02, 0.045, 0.005)
+    emigration_vals = np.arange(0.2, 0.45, 0.05)
     forest_vals = [False, True]
-    param_list = list(product(r_vals, e_vals, forest_vals))
-    df = pd.DataFrame(columns=['r', 'eK', 'model', 'score'])
+    param_list = list(product(growth_vals, emigration_vals, forest_vals))
+    df = pd.DataFrame(columns=['growth', 'emigration', 'model', 'score'])
     n_cores = mp.cpu_count() - 1
     pool = mp.Pool(n_cores)
     print(f'Running {len(param_list)} simulations. This may take a while ...')
-    res = pool.map(test_model, param_list)
+    scores = pool.map(test_model, param_list)
     pool.close()
-    for i in res:
+    for i in scores:
         df = df.append(i, ignore_index=True)
     df = df.sort_values('score')
     df.to_csv('results/sim_scores.csv')
 
+    null_model = Model(5000, ORIGIN, 0.025, 0.3, False)
+    null_model.run()
+    null_model.get_score(SITES)
+    null_model.write('results/rasters/null_model.asc')
+    null_model.sites['model'] = 'null'
 
-    m1 = Model(5000, ORIGIN, 0.025, 0.3, False)
-    m1.run()
-    m1.get_score(SITES)
-    m1.write('results/rasters/null_model.asc')
-    m1.sites['model'] = 'null'
+    forest_model = Model(5000, ORIGIN, 0.025, 0.3, True)
+    forest_model.run()
+    forest_model.get_score(SITES)
+    forest_model.write('results/rasters/forest_model.asc')
+    forest_model.sites['model'] = 'forest'
 
-    m2 = Model(5000, ORIGIN, 0.025, 0.3, True)
-    m2.run()
-    m2.get_score(SITES)
-    m2.write('results/rasters/forest_model.asc')
-    m2.sites['model'] = 'forest'
-
-    df_sim_dates = pd.concat([m1.sites, m2.sites])
+    df_sim_dates = pd.concat([null_model.sites, forest_model.sites])
     df_sim_dates.to_csv('results/sim_dates.csv')
 
 
